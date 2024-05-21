f30141a985975e75d67603a01f3c0085e4990a7e
Mehb101 <gulammehboobq@gmail.com>
2024-05-20 17:24:46 -0700

    done

---
+++ FamilyModel/Search.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyModel;
+[Table("Search")]
+
+public partial class Search
+{
+    [Key]
+
+    public int Cost { get; set; }
+
+    public string Size { get; set; } = null!;
+    public string Characteristic { get; set; } = null!;
+}
